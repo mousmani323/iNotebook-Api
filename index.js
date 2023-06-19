ed0824f216588c646b3f53be23bd58c014afe353
Muhammad Omar <mousmani323@gmail.com>
2023-06-19 15:18:27 +0500

    3rd

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const connectToMongo = require('./db');
 const express = require('express');
 
 connectToMongo();
-const app = express()
+var app = express()
+var cors = require('cors')
 const port = 5000
+
+
+app.use(cors())
 app.use(express.json())
 
 //available routes
